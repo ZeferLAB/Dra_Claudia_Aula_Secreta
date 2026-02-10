173d424ddcb3f2767171cc434b46eb0a5f2ba1ad
José Fernando <horadelancar@gmail.com>
2026-02-10 11:10:47 -0300

    Fix layout issue: remove basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. Força o Next.js a criar a pasta "out" (HTML estático)
+  output: "export",
+
+  // 2. Avisa que o site vai morar numa subpasta (para carregar CSS/JS corretamente)
+  // basePath: "/cadastro-smi-c-crm",
+
+  // 3. Desativa a otimização de imagem do servidor (necessário para site estático)
+  images: {
+    unoptimized: true,
+  },
+
+  // 4. Cria pastas com index.html (ex: /sobre/index.html) para evitar erro 404
+  trailingSlash: true,
 };
 
 export default nextConfig;
